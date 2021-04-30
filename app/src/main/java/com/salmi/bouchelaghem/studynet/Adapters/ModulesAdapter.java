78be268200931d41958d21ce43522429e6a2adfe
itsAbdou <its.abdou.ss@gmail.com>
2021-04-30 21:28:35 +0100

    Subjects fragment + subjects adapter.

---
+++ app/src/main/java/com/salmi/bouchelaghem/studynet/Adapters/ModulesAdapter.java
@@ -0,0 +1,79 @@
+package com.salmi.bouchelaghem.studynet.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.salmi.bouchelaghem.studynet.Models.Module;
+import com.salmi.bouchelaghem.studynet.R;
+import com.salmi.bouchelaghem.studynet.databinding.LayoutSubjectBinding;
+
+import java.util.List;
+
+public class ModulesAdapter extends RecyclerView.Adapter<ModulesAdapter.ViewHolder> {
+
+    private List<Module> modules;
+    private final Context context;
+
+    public ModulesAdapter(Context context) {
+        this.context = context;
+    }
+
+    public ModulesAdapter(List<Module> modules, Context context) {
+        this.modules = modules;
+        this.context = context;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutSubjectBinding binding = LayoutSubjectBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        ViewHolder holder = new ViewHolder(binding);
+
+        holder.binding.subjectMainLayout.setOnClickListener(v -> {
+            Module module = modules.get(holder.getAdapterPosition());
+            Navigation.createNavigateOnClickListener(R.id.action_nav_subjects_to_subjectDetailsFragment).onClick(holder.binding.getRoot());
+            // TODO: Send the module object as a safe arg
+        });
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Module module = modules.get(position);
+
+        holder.binding.txtSubjectCode.setText(module.getCode());
+        holder.binding.txtSubjectName.setText(module.getName());
+        holder.binding.txtSubjectTypes.setText("");
+        for (int i=0; i<module.getTypes().size()-1; i++){
+            holder.binding.txtSubjectTypes.append(module.getTypes().get(i) + ", ");
+        }
+        holder.binding.txtSubjectTypes.append(module.getTypes().get(module.getTypes().size()-1));
+    }
+
+    @Override
+    public int getItemCount() {
+        return modules.size();
+    }
+
+    public void setModules(List<Module> modules) {
+        this.modules = modules;
+        notifyDataSetChanged();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder{
+
+        LayoutSubjectBinding binding;
+
+        public ViewHolder(LayoutSubjectBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+}
--- app/src/main/java/com/salmi/bouchelaghem/studynet/Fragments/SubjectsFragment.java
+++ app/src/main/java/com/salmi/bouchelaghem/studynet/Fragments/SubjectsFragment.java
@@ -1,41 +1,83 @@
 package com.salmi.bouchelaghem.studynet.Fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
 import com.salmi.bouchelaghem.studynet.Activities.NavigationActivity;
-import com.salmi.bouchelaghem.studynet.R;
-import com.salmi.bouchelaghem.studynet.databinding.FragmentHomeworksBinding;
+import com.salmi.bouchelaghem.studynet.Adapters.ModulesAdapter;
+import com.salmi.bouchelaghem.studynet.Models.Module;
+import com.salmi.bouchelaghem.studynet.Models.Student;
+import com.salmi.bouchelaghem.studynet.Utils.CurrentUser;
+import com.salmi.bouchelaghem.studynet.Utils.TestAPI;
 import com.salmi.bouchelaghem.studynet.databinding.FragmentSubjectsBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SubjectsFragment extends Fragment {
 
     private FragmentSubjectsBinding binding;
 
+    // Recycler view
+    private List<Module> modules;
+    private ModulesAdapter adapter;
+
+    private Student currentStudent;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentSubjectsBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
+
+        currentStudent = CurrentUser.getInstance().getCurrentStudent();
+        initRecView();
 
         // Hide filter button
         NavigationActivity context = (NavigationActivity) getActivity();
         assert context != null;
         context.btnFilter.setVisibility(View.GONE);
 
-//        binding.included.subjectMainLayout.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Navigation.findNavController(view).navigate(R.id.action_nav_subjects_to_subjectDetailsFragment);
-//            }
-//        });
-
         return view;
+    }
+
+    private void initRecView() {
+        modules = new ArrayList<>();
+        binding.subjectRecView.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new ModulesAdapter(getContext());
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (modules != null && modules.isEmpty()) {
+            // if its the first time we launch the activity so go ahead and get the data from the database
+            getModules(currentStudent.getSection().getCode());
+        } else { // if we already retrieved the data from the database, just keep using them
+            adapter.setModules(modules);
+            binding.subjectRecView.setAdapter(adapter);
+            binding.subjectRecView.setVisibility(View.VISIBLE);
+            binding.emptyMsg.setVisibility(View.GONE);
+        }
+    }
+
+    // Get the current student's modules
+    private void getModules(String section) {
+        modules = TestAPI.getInstance().getModules();
+        if (!modules.isEmpty()) {
+            adapter.setModules(modules);
+            binding.subjectRecView.setAdapter(adapter);
+            binding.subjectRecView.setVisibility(View.VISIBLE);
+            binding.emptyMsg.setVisibility(View.GONE);
+        } else {
+            binding.subjectRecView.setVisibility(View.GONE);
+            binding.emptyMsg.setVisibility(View.VISIBLE);
+        }
     }
 }
