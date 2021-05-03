3d4dce1910c00b47caa09cb211ac23abef474034
Alphashow <lastblast@outlook.fr>
2021-05-03 21:36:14 +0100

    Added sections ArrayList to teacher.

--- app/src/main/java/com/salmi/bouchelaghem/studynet/Models/Teacher.java
+++ app/src/main/java/com/salmi/bouchelaghem/studynet/Models/Teacher.java
@@ -2,19 +2,22 @@ package com.salmi.bouchelaghem.studynet.Models;
 
 import org.threeten.bp.ZonedDateTime;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class Teacher extends User {
 
     private String grade;
+    private ArrayList<String> sections;
 
     public Teacher() {
     }
 
-    public Teacher(int id, String email, String firstName, String lastName, ZonedDateTime dateJoined, String grade) {
+    public Teacher(int id, String email, String firstName, String lastName, ZonedDateTime dateJoined, String grade,ArrayList<String> sections) {
         super(id, email, firstName, lastName, dateJoined);
         this.grade = grade;
+        this.sections = sections;
     }
 
     public String getGrade() {
