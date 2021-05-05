3c8aefe9769bfc3c8631ef2550718edc53175ddf
itsAbdou <its.abdou.ss@gmail.com>
2021-05-05 17:29:30 +0100

    Add assignment layout.

---
+++ app/src/main/java/com/salmi/bouchelaghem/studynet/Activities/AddAssignmentActivity.java
@@ -0,0 +1,16 @@
+package com.salmi.bouchelaghem.studynet.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.salmi.bouchelaghem.studynet.R;
+
+public class AddAssignmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_assignment);
+    }
+}
