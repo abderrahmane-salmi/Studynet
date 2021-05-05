1cacc0a4dada0ba5de5ad5880ad0c78ba756c98d
itsAbdou <its.abdou.ss@gmail.com>
2021-05-05 22:29:00 +0100

    Update teacher activity + Assignments rec view.

--- app/src/main/java/com/salmi/bouchelaghem/studynet/Activities/AddTeacherActivity.java
+++ app/src/main/java/com/salmi/bouchelaghem/studynet/Activities/AddTeacherActivity.java
@@ -1,16 +1,26 @@
 package com.salmi.bouchelaghem.studynet.Activities;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.salmi.bouchelaghem.studynet.Adapters.AssignmentsAdapter;
+import com.salmi.bouchelaghem.studynet.Adapters.TeachersAdapter;
 import com.salmi.bouchelaghem.studynet.Models.Assignment;
 import com.salmi.bouchelaghem.studynet.Models.Department;
 import com.salmi.bouchelaghem.studynet.Models.Section;
@@ -23,6 +33,8 @@ import com.salmi.bouchelaghem.studynet.databinding.ActivityAddTeacherBinding;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator;
 
 public class AddTeacherActivity extends AppCompatActivity {
 
@@ -52,6 +64,8 @@ public class AddTeacherActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+        initRecView();
+
         // Get the action type (Add/Update)
         Intent intent = getIntent();
         String action = intent.getStringExtra(Utils.ACTION);
@@ -60,10 +74,10 @@ public class AddTeacherActivity extends AppCompatActivity {
         setupGradesSpinner();
         setupDepartmentsSpinner();
 
-        switch (action){
+        switch (action) {
             case Utils.ACTION_ADD:
                 binding.btnNext.setOnClickListener(v -> {
-                    switch (step){
+                    switch (step) {
                         case 1: // Step1: Fill the teacher's basic info
 //                            if (validateFirstName() & validateLastName() & validateEmail() & validatePassword() & grade != null & department != null & sectionsSelected){
 //                                step = 2;
@@ -127,23 +141,57 @@ public class AddTeacherActivity extends AppCompatActivity {
                 fillFields(teacher);
                 // Save button
                 binding.btnNext.setOnClickListener(v -> {
-                    if (validateFirstName() & validateLastName() & validateEmail() & grade != null){
-                        String firstName = binding.txtFirstName.getEditText().getText().toString().trim();
-                        String lastName = binding.txtLastName.getEditText().getText().toString().trim();
-                        String email = binding.txtEmail.getEditText().getText().toString().trim();
+                    switch (step) {
+                        case 1: // Step1: Fill the teacher's basic info
+//                            if (validateFirstName() & validateLastName() & validateEmail() & validatePassword() & grade != null & department != null & sectionsSelected){
+//                                step = 2;
+//                                binding.teacherInfoLayout.setVisibility(View.GONE);
+//                                binding.assignmentsRecView.setVisibility(View.VISIBLE);
+//                                binding.btnAdd.setVisibility(View.VISIBLE);
+//                                // Show back button
+//                                binding.btnStepBack.setVisibility(View.VISIBLE);
+//                                // Show empty msg
+//                                binding.emptyMsg.setVisibility(View.VISIBLE);
 
-                        if (!teacher.getFirstName().equals(firstName) ||
-                                !teacher.getLastName().equals(lastName) ||
-                                !teacher.getEmail().equals(email) ||
-                                !teacher.getGrade().equals(grade)){
+                            // (for later)
+//                if (!teacher.getFirstName().equals(firstName) ||
+//                        !teacher.getLastName().equals(lastName) ||
+//                        !teacher.getEmail().equals(email) ||
+//                        !teacher.getGrade().equals(grade)){
+//                    Toast.makeText(AddTeacherActivity.this, "Save", Toast.LENGTH_SHORT).show();
+//                } else {
+//                    Toast.makeText(this, getString(R.string.no_changes_msg), Toast.LENGTH_SHORT).show();
+//                }
+//                            } else {
+//                                if (grade == null){
+//                                    binding.txtGradeLayout.setError(getString(R.string.empty_grade_msg));
+//                                }
+//                                if (department == null){
+//                                    binding.txtDepartmentLayout.setError(getString(R.string.empty_msg1));
+//                                }
+//                                if (grade == null){
+//                                    binding.txtSectionsList.setError("");
+//                                }
+//                            }
+                            // Go to the next step
+                            step = 2;
+                            binding.teacherInfoLayout.setVisibility(View.GONE);
+                            binding.assignmentsRecView.setVisibility(View.VISIBLE);
+                            binding.btnAdd.setVisibility(View.VISIBLE);
+                            // Show back button
+                            binding.btnStepBack.setVisibility(View.VISIBLE);
+                            // Show current teacher's assignments
+                            getAssignments(teacher.getId());
+                            break;
+                        case 2: // Step 2: Add assignments
+                            // Save the teacher's info
+                            String firstName = binding.txtFirstName.getEditText().getText().toString().trim();
+                            String lastName = binding.txtLastName.getEditText().getText().toString().trim();
+                            String email = binding.txtEmail.getEditText().getText().toString().trim();
+                            String password = binding.txtPassword.getEditText().getText().toString().trim();
+
                             Toast.makeText(AddTeacherActivity.this, "Save", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(this, getString(R.string.no_changes_msg), Toast.LENGTH_SHORT).show();
-                        }
-                    } else {
-                        if (grade == null){
-                            binding.txtGradeLayout.setError(getString(R.string.empty_grade_msg));
-                        }
+                            break;
                     }
                 });
 
@@ -219,7 +267,7 @@ public class AddTeacherActivity extends AppCompatActivity {
         binding.btnClose.setOnClickListener(v -> finish());
 
         binding.btnStepBack.setOnClickListener(v -> {
-            if (step == 2){
+            if (step == 2) {
                 step = 1;
                 // Hide step2
                 binding.assignmentsRecView.setVisibility(View.GONE);
@@ -230,6 +278,28 @@ public class AddTeacherActivity extends AppCompatActivity {
                 binding.teacherInfoLayout.setVisibility(View.VISIBLE);
             }
         });
+    }
+
+    private void initRecView() {
+        assignments = new ArrayList<>();
+        binding.assignmentsRecView.setLayoutManager(new LinearLayoutManager(AddTeacherActivity.this));
+        binding.assignmentsRecView.addItemDecoration(new DividerItemDecoration(AddTeacherActivity.this, LinearLayout.VERTICAL));
+        adapter = new AssignmentsAdapter(AddTeacherActivity.this);
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(adminCallBack);
+        itemTouchHelper.attachToRecyclerView(binding.assignmentsRecView);
+    }
+
+    private void getAssignments(int teacherId) {
+        assignments = new ArrayList<>(TestAPI.getInstance().getAssignments());
+        if (!assignments.isEmpty()){
+            adapter.setAssignments(assignments);
+            binding.assignmentsRecView.setAdapter(adapter);
+            binding.assignmentsRecView.setVisibility(View.VISIBLE);
+            binding.emptyMsg.setVisibility(View.GONE);
+        } else {
+            binding.assignmentsRecView.setVisibility(View.GONE);
+            binding.emptyMsg.setVisibility(View.VISIBLE);
+        }
     }
 
     // Get all the sections in the selected department
@@ -243,14 +313,14 @@ public class AddTeacherActivity extends AppCompatActivity {
         selectedSections = new ArrayList<>();
 
         // Fill the sections array
-        for (int i=0; i<sectionList.size(); i++){
+        for (int i = 0; i < sectionList.size(); i++) {
             sectionsArray[i] = sectionList.get(i).getCode();
         }
     }
 
     private void setupDepartmentsSpinner() {
         departments = new ArrayList<>();
-        for (Department department: TestAPI.getInstance().getDepartments()){
+        for (Department department : TestAPI.getInstance().getDepartments()) {
             departments.add(department.getCode());
         }
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(AddTeacherActivity.this, R.layout.dropdown_item, departments);
@@ -285,32 +355,32 @@ public class AddTeacherActivity extends AppCompatActivity {
 
         // Set the selected sections to the text view
         int nbSections = selectedSections.size();
-        if (nbSections == 0){
+        if (nbSections == 0) {
             binding.txtSectionsList.setText(R.string.no_sections_selected_msg);
-        } else if (nbSections == 1){
+        } else if (nbSections == 1) {
             binding.txtSectionsList.setText(selectedSections.get(0));
         } else {
             binding.txtSectionsList.setText("");
-            for (int i = 0; i< selectedSections.size()-1; i++){
+            for (int i = 0; i < selectedSections.size() - 1; i++) {
                 // Show the selected sections in the text view
                 binding.txtSectionsList.append(selectedSections.get(i) + ", ");
             }
-            binding.txtSectionsList.append(selectedSections.get(nbSections-1));
+            binding.txtSectionsList.append(selectedSections.get(nbSections - 1));
         }
 
     }
 
     private void setSelectedSections(List<String> selectedSections) {
-        for (int i=0; i<sectionsArray.length; i++){
+        for (int i = 0; i < sectionsArray.length; i++) {
             sectionsStates[i] = selectedSections.contains(sectionsArray[i]);
         }
     }
 
     // Validation methods
-    public boolean validateFirstName(){
+    public boolean validateFirstName() {
         String firstName = binding.txtFirstName.getEditText().getText().toString().trim();
 
-        if (firstName.isEmpty()){
+        if (firstName.isEmpty()) {
             binding.txtFirstName.setError(getString(R.string.first_name_msg));
             return false;
         } else {
@@ -319,10 +389,10 @@ public class AddTeacherActivity extends AppCompatActivity {
         }
     }
 
-    public boolean validateLastName(){
+    public boolean validateLastName() {
         String lastName = binding.txtLastName.getEditText().getText().toString().trim();
 
-        if (lastName.isEmpty()){
+        if (lastName.isEmpty()) {
             binding.txtLastName.setError(getString(R.string.last_name_msg));
             return false;
         } else {
@@ -331,10 +401,10 @@ public class AddTeacherActivity extends AppCompatActivity {
         }
     }
 
-    public boolean validateEmail(){
+    public boolean validateEmail() {
         String email = binding.txtEmail.getEditText().getText().toString().trim();
 
-        if (email.isEmpty()){
+        if (email.isEmpty()) {
             binding.txtEmail.setError(getString(R.string.email_msg1));
             return false;
         } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
@@ -346,10 +416,10 @@ public class AddTeacherActivity extends AppCompatActivity {
         }
     }
 
-    public boolean validatePassword(){
+    public boolean validatePassword() {
         String password = binding.txtPassword.getEditText().getText().toString().trim();
 
-        if (password.isEmpty()){
+        if (password.isEmpty()) {
             binding.txtPassword.setError(getString(R.string.password_msg));
             return false;
         } else if (password.length() < 6) {
@@ -360,4 +430,62 @@ public class AddTeacherActivity extends AppCompatActivity {
             return true;
         }
     }
+
+    // Admin call back (swipe feature)
+    // Swipe to delete and edit in the recycler view
+    ItemTouchHelper.SimpleCallback adminCallBack = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+
+            int position = viewHolder.getAdapterPosition();
+            Assignment currentAssignment = adapter.getAssignments().get(position);
+
+            switch (direction){
+                case ItemTouchHelper.LEFT: // Swipe left to right <- : Delete item
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(AddTeacherActivity.this);
+                    builder.setMessage(R.string.are_you_sure);
+                    builder.setPositiveButton(R.string.yes, (dialog, which) -> {
+                        assignments.remove(currentAssignment);
+                        adapter.getAssignments().remove(currentAssignment);
+                        adapter.notifyItemRemoved(position);
+                        Toast.makeText(AddTeacherActivity.this, getString(R.string.assignment_deleted_msg), Toast.LENGTH_SHORT).show();
+                    });
+                    builder.setNegativeButton(R.string.no, (dialog, which) -> {
+                        // Do Nothing
+                        adapter.notifyItemChanged(position); // To reset the item on the screen
+                    });
+                    builder.create().show();
+                    break;
+                case ItemTouchHelper.RIGHT: // Swipe right to left -> : Edit item
+                    Intent intent1 = new Intent(AddTeacherActivity.this, AddAssignmentActivity.class);
+                    intent1.putExtra(Utils.ACTION, Utils.ACTION_UPDATE);
+                    // TODO: get the teacher's id
+                    intent1.putExtra(Utils.ID, 1);
+                    intent1.putExtra(Utils.SECTIONS, selectedSections);
+                    startActivity(intent1);
+                    adapter.notifyItemChanged(position); // To reset the item on the screen
+                    break;
+            }
+        }
+
+        @Override
+        public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+
+            new RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                    .addSwipeLeftBackgroundColor(ContextCompat.getColor(AddTeacherActivity.this, R.color.red))
+                    .addSwipeLeftActionIcon(R.drawable.ic_delete)
+                    .addSwipeRightBackgroundColor(ContextCompat.getColor(AddTeacherActivity.this, R.color.green))
+                    .addSwipeRightActionIcon(R.drawable.ic_modify)
+                    .create()
+                    .decorate();
+
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+    };
 }
