6aabb7b2093714da79dbb0c3ec988b20c90692a8
Alphashow <lastblast@outlook.fr>
2021-05-12 17:24:17 +0100

    Now retrieves user data from the API when launching app.

--- app/src/main/java/com/salmi/bouchelaghem/studynet/Activities/SplashActivity.java
+++ app/src/main/java/com/salmi/bouchelaghem/studynet/Activities/SplashActivity.java
@@ -15,15 +15,20 @@ import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.content.ContextCompat;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.salmi.bouchelaghem.studynet.Models.Admin;
 import com.salmi.bouchelaghem.studynet.Models.Student;
 import com.salmi.bouchelaghem.studynet.Models.Teacher;
 import com.salmi.bouchelaghem.studynet.R;
 import com.salmi.bouchelaghem.studynet.Utils.CurrentUser;
+import com.salmi.bouchelaghem.studynet.Utils.Serializers;
 import com.salmi.bouchelaghem.studynet.Utils.StudynetAPI;
 import com.salmi.bouchelaghem.studynet.Utils.Utils;
 import com.salmi.bouchelaghem.studynet.databinding.ActivitySplashBinding;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -31,6 +36,7 @@ public class SplashActivity extends AppCompatActivity {
 
     private ActivitySplashBinding binding;
     private SharedPreferences sharedPreferences;
+    private CurrentUser currentUser;
     private boolean loggedIn;
     private StudynetAPI api;
 
@@ -48,7 +54,7 @@ public class SplashActivity extends AppCompatActivity {
 
         // Init our api, this will implement the code of all the methods in the interface.
         api = retrofit.create(StudynetAPI.class);
-
+        currentUser = CurrentUser.getInstance();
         //Get the shared preferences.
         sharedPreferences = getApplicationContext().getSharedPreferences(Utils.SHARED_PREFERENCES_USER_DATA,MODE_PRIVATE);
         // Set light theme as the default theme
@@ -64,22 +70,23 @@ public class SplashActivity extends AppCompatActivity {
         NetworkInfo network = connectivityManager.getActiveNetworkInfo();
         if (network == null) { // No internet
 
-            binding.mainLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.white));
+            binding.mainLayout.setBackgroundColor(getColor(R.color.white));
+
             binding.noInternetMsg.setVisibility(View.VISIBLE);
 
         } else {
 
             new Handler().postDelayed(() -> {
                 //Check if the user is already logged in using shared preferences.
-                if (sharedPreferences.getBoolean(Utils.SHARED_PREFERENCES_LOGGED_IN,false) && loadUserData())
+                if (sharedPreferences.getBoolean(Utils.SHARED_PREFERENCES_LOGGED_IN,false))
                 {
-                    startActivity(new Intent(SplashActivity.this, NavigationActivity.class));
+                    loadUserData();
                 }
                 else
                 {
                     startActivity(new Intent(SplashActivity.this, LoginActivity.class));
                 }
-                finish();
+                //finish();
             }, 1000);
 
         }
@@ -90,9 +97,71 @@ public class SplashActivity extends AppCompatActivity {
     {
         CurrentUser currentUser = CurrentUser.getInstance();
         //The user is already logged in, we determine which type of user this is.
+        //Get the token from shared preferences
         String token = sharedPreferences.getString(Utils.SHARED_PREFERENCES_TOKEN, "");
+        currentUser.setToken(token);
         //Send the token to the API to receive the user data.
+        Call<JsonObject> call = api.getUserData("Token " + token);
+        call.enqueue(new Callback<JsonObject>() {
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                switch(response.code())
+                {
+                    case Utils.HttpResponses.HTTP_200_OK:
+                        JsonObject responseData = response.body();
+                        if (responseData.has(Utils.STUDENT_ACCOUNT))
+                        {
+                            //It's a student
+                            loadStudent(responseData.getAsJsonObject(Utils.STUDENT_ACCOUNT));
+                        }
+                        else
+                        {
+                            if (responseData.has(Utils.TEACHER_ACCOUNT))
+                            {
+                                //It's a teacher
+                                loadTeacher(responseData.getAsJsonObject(Utils.TEACHER_ACCOUNT));
+                            }
+                            else
+                            {
+                                if (responseData.has(Utils.ADMIN_ACCOUNT))
+                                {
+                                    //It's an administrator
+                                    loadAdmin(responseData.getAsJsonObject(Utils.ADMIN_ACCOUNT));
+                                }
+                                else
+                                {
+                                    //Unexpected response from the server.
+                                    Toast.makeText(SplashActivity.this, getString(R.string.unknown_error), Toast.LENGTH_SHORT).show();
+                                    break;
+                                }
+                            }
+                        }
+                        startActivity(new Intent(SplashActivity.this, NavigationActivity.class));
+                        break;
+                    case Utils.HttpResponses.HTTP_401_UNAUTHORIZED:
+                        //Token is invalid, disconnect the user.
+                        SharedPreferences.Editor prefsEditor = sharedPreferences.edit();
+                        prefsEditor.putBoolean(Utils.SHARED_PREFERENCES_LOGGED_IN,false);
+                        prefsEditor.apply();
+                        Toast.makeText(SplashActivity.this, getString(R.string.session_expired), Toast.LENGTH_LONG).show();
+                        break;
+                    default:
+                        Toast.makeText(SplashActivity.this, getString(R.string.unknown_error), Toast.LENGTH_LONG).show();
+                        binding.mainLayout.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.white));
+                        binding.noInternetMsg.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+                Toast.makeText(SplashActivity.this, getString(R.string.connection_failed), Toast.LENGTH_LONG).show();
+                binding.mainLayout.setBackgroundColor(getColor(R.color.white));
+                binding.noInternetMsg.setVisibility(View.VISIBLE);
+
+            }
+        });
         return false;
+    }
 //        switch(userType)
 //        {
 //            case Utils.STUDENT_ACCOUNT:
@@ -120,5 +189,18 @@ public class SplashActivity extends AppCompatActivity {
 //                return false;
 //        }
 //  }
+
+
+    private void loadStudent(JsonObject student) {
+        currentUser.setUserType(Utils.STUDENT_ACCOUNT);
+        currentUser.setCurrentStudent(Serializers.StudentDeserializer(student));
+    }
+    private void loadTeacher(JsonObject teacher) {
+        currentUser.setUserType(Utils.TEACHER_ACCOUNT);
+        currentUser.setCurrentTeacher(Serializers.TeacherDeserializer(teacher));
+    }
+    private void loadAdmin(JsonObject admin) {
+        currentUser.setUserType(Utils.ADMIN_ACCOUNT);
+        currentUser.setCurrentAdmin(Serializers.AdminDeserializer(admin));
     }
 }
