bc05511855ced71f000544316011feb51b6ba5b1
itsAbdou <its.abdou.ss@gmail.com>
2021-05-19 23:40:46 +0100

    Added loading animation to sign up + fixed some warnings.

--- app/src/main/java/com/salmi/bouchelaghem/studynet/Activities/SignUpActivity.java
+++ app/src/main/java/com/salmi/bouchelaghem/studynet/Activities/SignUpActivity.java
@@ -17,9 +17,9 @@ import com.salmi.bouchelaghem.studynet.Models.Section;
 import com.salmi.bouchelaghem.studynet.Models.Specialty;
 import com.salmi.bouchelaghem.studynet.R;
 import com.salmi.bouchelaghem.studynet.Utils.CurrentUser;
+import com.salmi.bouchelaghem.studynet.Utils.CustomLoadingDialog;
 import com.salmi.bouchelaghem.studynet.Utils.Serializers;
 import com.salmi.bouchelaghem.studynet.Utils.StudynetAPI;
-import com.salmi.bouchelaghem.studynet.Utils.TestAPI;
 import com.salmi.bouchelaghem.studynet.Utils.Utils;
 import com.salmi.bouchelaghem.studynet.databinding.ActivitySignUpBinding;
 
@@ -47,12 +47,10 @@ public class SignUpActivity extends AppCompatActivity {
     private boolean groupSelected = false;
 
     // Fields
-    private String department, speciality, section;
     private int group;
     private Section studentSection;
 
-    // Test Api
-    TestAPI testAPI;
+    private CustomLoadingDialog loadingDialog;
 
     // Studynet Api
     private StudynetAPI api;
@@ -64,8 +62,7 @@ public class SignUpActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
-        // Test Api
-        testAPI = TestAPI.getInstance();
+        loadingDialog = new CustomLoadingDialog(SignUpActivity.this);
 
         // Init retrofit
         Retrofit retrofit = new Retrofit.Builder()
@@ -85,7 +82,6 @@ public class SignUpActivity extends AppCompatActivity {
         binding.txtDepartment.setOnItemClickListener((parent, view14, position, id) -> {
             // Get the selected item
             departmentSelected = true;
-            department = departments.get(position).getName();
             binding.departmentTextInputLayout.setError(null);
 
             // Disable other spinners
@@ -109,7 +105,6 @@ public class SignUpActivity extends AppCompatActivity {
         binding.txtSpeciality.setOnItemClickListener((parent, view13, position, id) -> {
             // Get the selected item
             specialitySelected = true;
-            speciality = specialties.get(position).getName();
             binding.specialityTextInputLayout.setError(null);
 
             // Disable other spinners
@@ -130,7 +125,6 @@ public class SignUpActivity extends AppCompatActivity {
             // Get the selected item
             sectionSelected = true;
             studentSection = sections.get(position);
-            section = studentSection.getCode();
             binding.sectionTextInputLayout.setError(null);
 
             // Disable other spinners
@@ -146,7 +140,7 @@ public class SignUpActivity extends AppCompatActivity {
         binding.txtGroup.setOnItemClickListener((parent, view1, position, id) -> {
             // Get the selected item
             groupSelected = true;
-            group = position+1;
+            group = position + 1;
             binding.groupTextInputLayout.setError(null);
         });
 
@@ -154,7 +148,7 @@ public class SignUpActivity extends AppCompatActivity {
 
     private void setupGroupsSpinner(int nbGroups) {
         List<String> groups = new ArrayList<>();
-        for (int i=1; i<=nbGroups; i++){
+        for (int i = 1; i <= nbGroups; i++) {
             groups.add(String.valueOf(i));
         }
 
@@ -169,12 +163,12 @@ public class SignUpActivity extends AppCompatActivity {
         call.enqueue(new Callback<List<Section>>() {
             @Override
             public void onResponse(@NonNull Call<List<Section>> call, @NonNull Response<List<Section>> response) {
-                if (response.isSuccessful()){
+                if (response.isSuccessful()) {
                     sections = response.body();
 
                     // Get names
                     List<String> sectionsNames = new ArrayList<>();
-                    for (Section sec:sections){
+                    for (Section sec : sections) {
                         sectionsNames.add(sec.getCode());
                     }
 
@@ -182,13 +176,13 @@ public class SignUpActivity extends AppCompatActivity {
                     ArrayAdapter<String> sectionsAdapter = new ArrayAdapter<>(SignUpActivity.this, R.layout.dropdown_item, sectionsNames);
                     binding.txtSection.setAdapter(sectionsAdapter);
                 } else {
-                    Toast.makeText(SignUpActivity.this, getString(R.string.error)+response.code(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(SignUpActivity.this, getString(R.string.error) + response.code(), Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
             public void onFailure(@NonNull Call<List<Section>> call, @NonNull Throwable t) {
-                Toast.makeText(SignUpActivity.this, getString(R.string.error)+t.getMessage(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(SignUpActivity.this, getString(R.string.error) + t.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -199,12 +193,12 @@ public class SignUpActivity extends AppCompatActivity {
         call.enqueue(new Callback<List<Specialty>>() {
             @Override
             public void onResponse(@NonNull Call<List<Specialty>> call, @NonNull Response<List<Specialty>> response) {
-                if (response.isSuccessful()){
+                if (response.isSuccessful()) {
                     specialties = response.body();
 
                     // Get names
                     List<String> specialitiesNames = new ArrayList<>();
-                    for (Specialty s:specialties){
+                    for (Specialty s : specialties) {
                         specialitiesNames.add(s.getCode());
                     }
 
@@ -212,13 +206,13 @@ public class SignUpActivity extends AppCompatActivity {
                     ArrayAdapter<String> specialitiesAdapter = new ArrayAdapter<>(SignUpActivity.this, R.layout.dropdown_item, specialitiesNames);
                     binding.txtSpeciality.setAdapter(specialitiesAdapter);
                 } else {
-                    Toast.makeText(SignUpActivity.this, getString(R.string.error)+response.code(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(SignUpActivity.this, getString(R.string.error) + response.code(), Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
             public void onFailure(@NonNull Call<List<Specialty>> call, @NonNull Throwable t) {
-                Toast.makeText(SignUpActivity.this, getString(R.string.error)+t.getMessage(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(SignUpActivity.this, getString(R.string.error) + t.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -229,12 +223,12 @@ public class SignUpActivity extends AppCompatActivity {
         call.enqueue(new Callback<List<Department>>() {
             @Override
             public void onResponse(@NonNull Call<List<Department>> call, @NonNull Response<List<Department>> response) {
-                if (response.isSuccessful()){
+                if (response.isSuccessful()) {
                     departments = response.body();
 
                     // Get the names of the departments
                     List<String> departmentsNames = new ArrayList<>();
-                    for (Department department:departments){
+                    for (Department department : departments) {
                         departmentsNames.add(department.getName());
                     }
 
@@ -242,21 +236,21 @@ public class SignUpActivity extends AppCompatActivity {
                     ArrayAdapter<String> departmentsAdapter = new ArrayAdapter<>(SignUpActivity.this, R.layout.dropdown_item, departmentsNames);
                     binding.txtDepartment.setAdapter(departmentsAdapter);
                 } else {
-                    Toast.makeText(SignUpActivity.this, getString(R.string.error)+response.code(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(SignUpActivity.this, getString(R.string.error) + response.code(), Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
             public void onFailure(@NonNull Call<List<Department>> call, @NonNull Throwable t) {
-                Toast.makeText(SignUpActivity.this, getString(R.string.error)+t.getMessage(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(SignUpActivity.this, getString(R.string.error) + t.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    public boolean validateRegistrationNumber(){
+    public boolean validateRegistrationNumber() {
         String regNumber = binding.txtRegistrationNumber.getEditText().getText().toString().trim();
 
-        if (regNumber.isEmpty()){
+        if (regNumber.isEmpty()) {
             binding.txtRegistrationNumber.setError(getString(R.string.empty_reg_number_msg));
             return false;
         } else {
@@ -265,10 +259,10 @@ public class SignUpActivity extends AppCompatActivity {
         }
     }
 
-    public boolean validateFirstName(){
+    public boolean validateFirstName() {
         String firstName = binding.txtFirstName.getEditText().getText().toString().trim();
 
-        if (firstName.isEmpty()){
+        if (firstName.isEmpty()) {
             binding.txtFirstName.setError(getString(R.string.empty_first_name_msg));
             return false;
         } else {
@@ -277,10 +271,10 @@ public class SignUpActivity extends AppCompatActivity {
         }
     }
 
-    public boolean validateLastName(){
+    public boolean validateLastName() {
         String lastName = binding.txtLastName.getEditText().getText().toString().trim();
 
-        if (lastName.isEmpty()){
+        if (lastName.isEmpty()) {
             binding.txtLastName.setError(getString(R.string.empty_last_name_msg));
             return false;
         } else {
@@ -289,10 +283,10 @@ public class SignUpActivity extends AppCompatActivity {
         }
     }
 
-    public boolean validateEmail(){
+    public boolean validateEmail() {
         String email = binding.txtEmail.getEditText().getText().toString().trim();
 
-        if (email.isEmpty()){
+        if (email.isEmpty()) {
             binding.txtEmail.setError(getString(R.string.email_msg1));
             return false;
         } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
@@ -304,10 +298,10 @@ public class SignUpActivity extends AppCompatActivity {
         }
     }
 
-    public boolean validatePassword(){
+    public boolean validatePassword() {
         String password = binding.txtPassword.getEditText().getText().toString().trim();
 
-        if (password.isEmpty()){
+        if (password.isEmpty()) {
             binding.txtPassword.setError(getString(R.string.empty_password_msg));
             return false;
         } else if (password.length() < 6) {
@@ -319,11 +313,11 @@ public class SignUpActivity extends AppCompatActivity {
         }
     }
 
-    private void performSignup()
-    {
+    private void performSignup() {
         if (validateRegistrationNumber() & validateFirstName() & validateLastName() & validateEmail() & validatePassword() &
-                departmentSelected & specialitySelected & sectionSelected & groupSelected){
+                departmentSelected & specialitySelected & sectionSelected & groupSelected) {
 
+            loadingDialog.show();
             String registrationNumber = binding.txtRegistrationNumber.getEditText().getText().toString().trim();
             String firstName = binding.txtFirstName.getEditText().getText().toString().trim();
             String lastName = binding.txtLastName.getEditText().getText().toString().trim();
@@ -331,63 +325,66 @@ public class SignUpActivity extends AppCompatActivity {
             //Get the raw password (no trim either).
             String password = binding.txtPassword.getEditText().getText().toString();
             //Create the json data to send to the api.
-            JsonObject studentData = Serializers.studentSerializer(email,password,firstName,lastName,registrationNumber,studentSection.getCode(),group);
+            JsonObject studentData = Serializers.studentSerializer(email, password, firstName, lastName, registrationNumber, studentSection.getCode(), group);
             //Send the data to the API.
             Call<JsonObject> studentRegister = api.registerStudent(studentData);
             studentRegister.enqueue(new Callback<JsonObject>() {
                 @Override
                 public void onResponse(@NonNull Call<JsonObject> call, @NonNull Response<JsonObject> response) {
-                    switch (response.code())
-                    {
+                    switch (response.code()) {
                         case Utils.HttpResponses.HTTP_201_CREATED:
                             //The student has been successfully registered, we log him in using the data that was sent back by the API.
                             Utils.loginStudent(response.body());
                             //We save his data locally.
                             saveCurrentUser();
                             //Take him to the navigation activity.
-                            Toast.makeText(SignUpActivity.this, getString(R.string.student_registered), Toast.LENGTH_SHORT).show();
+                            Toast.makeText(SignUpActivity.this, getString(R.string.signed_up_msg), Toast.LENGTH_SHORT).show();
                             startActivity(new Intent(SignUpActivity.this, NavigationActivity.class));
+                            loadingDialog.dismiss();
                             finish();
                             break;
                         case Utils.HttpResponses.HTTP_400_BAD_REQUEST:
                             //The email is already taken.
                             binding.txtEmail.setError(getString(R.string.email_taken));
+                            loadingDialog.dismiss();
                             break;
                         default:
                             Toast.makeText(SignUpActivity.this, getString(R.string.unknown_error), Toast.LENGTH_SHORT).show();
+                            loadingDialog.dismiss();
+                            break;
                     }
                 }
 
                 @Override
-                public void onFailure(Call<JsonObject> call, Throwable t) {
-                    Toast.makeText(SignUpActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+                public void onFailure(@NonNull Call<JsonObject> call, @NonNull Throwable t) {
+                    loadingDialog.dismiss();
+                    Toast.makeText(SignUpActivity.this, getString(R.string.connection_failed), Toast.LENGTH_LONG).show();
                 }
             });
         } else {
-            if (!departmentSelected){
+            if (!departmentSelected) {
                 binding.departmentTextInputLayout.setError(getString(R.string.empty_department_msg));
             }
-            if (!specialitySelected){
+            if (!specialitySelected) {
                 binding.specialityTextInputLayout.setError(getString(R.string.empty_speciality_msg));
             }
             if (!sectionSelected) {
                 binding.sectionTextInputLayout.setError(getString(R.string.empty_section_msg));
             }
-            if (!groupSelected){
+            if (!groupSelected) {
                 binding.groupTextInputLayout.setError(getString(R.string.empty_group_msg));
             }
         }
     }
 
-    private void saveCurrentUser()
-    {
-        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(Utils.SHARED_PREFERENCES_USER_DATA,MODE_PRIVATE);
+    private void saveCurrentUser() {
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(Utils.SHARED_PREFERENCES_USER_DATA, MODE_PRIVATE);
         CurrentUser currentUser = CurrentUser.getInstance();
 
         SharedPreferences.Editor prefsEditor = sharedPreferences.edit();
         //Save the token
         prefsEditor.putString(Utils.SHARED_PREFERENCES_TOKEN, currentUser.getToken());
-        prefsEditor.putBoolean(Utils.SHARED_PREFERENCES_LOGGED_IN,true);
+        prefsEditor.putBoolean(Utils.SHARED_PREFERENCES_LOGGED_IN, true);
         prefsEditor.apply();
     }
 }
