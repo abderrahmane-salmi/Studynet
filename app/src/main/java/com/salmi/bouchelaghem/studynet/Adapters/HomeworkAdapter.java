9b87aa34274dc74c4a80dfccb4fe279dd259704c
itsAbdou <its.abdou.ss@gmail.com>
2021-05-23 17:27:43 +0100

    Programmed the homework check mark button.

--- app/src/main/java/com/salmi/bouchelaghem/studynet/Adapters/HomeworkAdapter.java
+++ app/src/main/java/com/salmi/bouchelaghem/studynet/Adapters/HomeworkAdapter.java
@@ -2,8 +2,10 @@ package com.salmi.bouchelaghem.studynet.Adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Paint;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
@@ -17,23 +19,17 @@ import com.salmi.bouchelaghem.studynet.R;
 import com.salmi.bouchelaghem.studynet.Utils.Utils;
 import com.salmi.bouchelaghem.studynet.databinding.LayoutHomeworkBinding;
 
-import org.threeten.bp.format.DateTimeFormatter;
-
 import java.util.List;
 
 public class HomeworkAdapter extends RecyclerView.Adapter<HomeworkAdapter.ViewHolder> {
 
     private List<Homework> homeworks;
     private final Context context;
+    private final String currentUserType;
 
-    public HomeworkAdapter(Context context) {
+    public HomeworkAdapter(Context context, String currentUserType) {
         this.context = context;
-    }
-
-    public HomeworkAdapter(List<Homework> homeworks, Context context) {
-        this.homeworks = homeworks;
-        this.context = context;
-        notifyDataSetChanged();
+        this.currentUserType = currentUserType;
     }
 
     @NonNull
@@ -49,37 +45,60 @@ public class HomeworkAdapter extends RecyclerView.Adapter<HomeworkAdapter.ViewHo
             context.startActivity(intent);
         });
 
-        holder.binding.btnCompleteHomework.setOnClickListener(v -> {
-            // TODO: read isChecked value from shared prefs
-            boolean isChecked = false;
-            if (isChecked){
-                // Mark the homework as unchecked
-                isChecked = false;
-                // TODO: save this value to shared prefs
-                // Play the anim in reverse
-                holder.binding.btnCompleteHomework.setSpeed(-1);
-                holder.binding.btnCompleteHomework.playAnimation();
+        if (currentUserType.equals(Utils.STUDENT_ACCOUNT)){
+            // If its a student then show the check button
+            holder.binding.checkMark.setVisibility(View.VISIBLE);
+            // Init the check button
+            holder.binding.btnCompleteHomework.setOnClickListener(v -> {
+                // Read the current homework's check state
+                Homework homework = homeworks.get(holder.getAdapterPosition());
+                boolean isChecked = readCheckState(homework.getId());
+                if (isChecked){
+                    // Mark the homework as unchecked
+                    saveCheckState(false, homework.getId());
+                    // Play the anim in reverse
+                    holder.binding.btnCompleteHomework.setSpeed(-1);
+                    holder.binding.btnCompleteHomework.playAnimation();
 //                    TODO: Add strike through text animation here (slowly)
-                // Change the text color
-                holder.binding.homeworkTitle.setPaintFlags(holder.binding.homeworkTitle.getPaintFlags() & (~Paint.STRIKE_THRU_TEXT_FLAG));
-                holder.binding.homeworkDate.setTextColor(context.getResources().getColor(R.color.secondary_text_color, null));
-                holder.binding.imageView2.setImageResource(R.drawable.ic_time);
-                DrawableCompat.setTint(DrawableCompat.wrap(holder.binding.imageView2.getDrawable()), ContextCompat.getColor(context, R.color.secondary_text_color));
-            } else {
-                // Mark the homework as unchecked
-                isChecked = true;
-                // TODO: save this value to shared prefs
-                // Play the animation
-                holder.binding.btnCompleteHomework.setSpeed(1);
-                holder.binding.btnCompleteHomework.playAnimation();
-                holder.binding.homeworkTitle.setPaintFlags(holder.binding.homeworkTitle.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
-                holder.binding.homeworkDate.setTextColor(context.getResources().getColor(R.color.primary_color, null));
-                holder.binding.imageView2.setImageResource(R.drawable.ic_checked);
-                DrawableCompat.setTint(DrawableCompat.wrap(holder.binding.imageView2.getDrawable()), ContextCompat.getColor(context, R.color.primary_color));
-            }
-        });
+                    // Change the text color
+                    holder.binding.homeworkTitle.setPaintFlags(holder.binding.homeworkTitle.getPaintFlags() & (~Paint.STRIKE_THRU_TEXT_FLAG));
+                    holder.binding.homeworkDate.setTextColor(context.getResources().getColor(R.color.secondary_text_color, null));
+                    holder.binding.imageView2.setImageResource(R.drawable.ic_time);
+                    DrawableCompat.setTint(DrawableCompat.wrap(holder.binding.imageView2.getDrawable()), ContextCompat.getColor(context, R.color.secondary_text_color));
+                } else {
+                    // Mark the homework as unchecked
+                    saveCheckState(true, homework.getId());
+                    // Play the animation
+                    holder.binding.btnCompleteHomework.setSpeed(1);
+                    holder.binding.btnCompleteHomework.playAnimation();
+                    holder.binding.homeworkTitle.setPaintFlags(holder.binding.homeworkTitle.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+                    holder.binding.homeworkDate.setTextColor(context.getResources().getColor(R.color.primary_color, null));
+                    holder.binding.imageView2.setImageResource(R.drawable.ic_checked);
+                    DrawableCompat.setTint(DrawableCompat.wrap(holder.binding.imageView2.getDrawable()), ContextCompat.getColor(context, R.color.primary_color));
+                }
+            });
+        }
 
         return holder;
+    }
+
+    // Checkmark methods
+    private void saveCheckState(boolean isChecked, int homeworkId) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(Utils.HOMEWORK, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        if (isChecked){
+            // Add the homework to the completed homeworks list
+            editor.putString(String.valueOf(homeworkId), String.valueOf(homeworkId));
+        } else {
+            // Remove the homework from the completed homeworks list
+            editor.remove(String.valueOf(homeworkId));
+        }
+        editor.apply();
+    }
+
+    private boolean readCheckState(int homeworkId) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(Utils.HOMEWORK, Context.MODE_PRIVATE);
+        return sharedPreferences.contains(String.valueOf(homeworkId));
     }
 
     @Override
@@ -88,7 +107,22 @@ public class HomeworkAdapter extends RecyclerView.Adapter<HomeworkAdapter.ViewHo
 
         holder.binding.homeworkTitle.setText(homework.getTitle());
         holder.binding.homeworkSubject.setText(homework.getModule());
+        // TODO: Why did we remove the formatter?
         holder.binding.homeworkDate.setText(homework.getDueDate());
+
+        // Read the current homework's check state
+        boolean isChecked = readCheckState(homework.getId());
+
+        if (isChecked){
+            // Show that the homework is checked
+            // Animation
+            holder.binding.btnCompleteHomework.setProgress((float) 0.9);
+            // Text
+            holder.binding.homeworkTitle.setPaintFlags(holder.binding.homeworkTitle.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+            holder.binding.homeworkDate.setTextColor(context.getResources().getColor(R.color.primary_color, null));
+            holder.binding.imageView2.setImageResource(R.drawable.ic_checked);
+            DrawableCompat.setTint(DrawableCompat.wrap(holder.binding.imageView2.getDrawable()), ContextCompat.getColor(context, R.color.primary_color));
+        }
     }
 
     @Override
